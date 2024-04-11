3b720b5fb267eaa86232accbf1f800f254e8a956
Kyle-Atienza <kylefrancisedison@gmail.com>
2024-04-11 17:00:26 +0800

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,23 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'primary': "#00BC89",
+      'light': "#F9EACD",
+      'dark': "#003135",
+      'base': "#3F6C4B",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      borderRadius: {
+        '4xl': '2.5rem'
+      }
     },
+
   },
   plugins: [],
 };
