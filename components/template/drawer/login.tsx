194e19c8086c3379e34cce785d5c893f50e0f65e
Kyle-Atienza <atienzakylefrancis@gmail.com>
2024-05-21 12:00:01 +0800

    fetch cart on login

--- components/template/drawer/login.tsx
+++ components/template/drawer/login.tsx
@@ -1,12 +1,20 @@
 import { Button } from "@/components/common/button";
 import { Input } from "@/components/common/forms/input";
+import { useOrdersStore } from "@/state/orders";
 import { useUserStore } from "@/state/user";
 
 export function Login() {
   const { signIn } = useUserStore();
+  const { getCart } = useOrdersStore();
 
   return (
-    <form action={signIn} className="">
+    <form
+      action={signIn}
+      onSubmit={() => {
+        getCart();
+      }}
+      className=""
+    >
       <div className="flex flex-col gap-spaced-sm">
         <Input label="Email Address" type="email" name="email" id="email" />
         <Input label="Password" type="password" name="password" id="password" />
