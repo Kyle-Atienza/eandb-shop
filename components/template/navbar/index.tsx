2918649be34879939f263ac129b3316e3ad596bd
Kyle-Atienza <atienzakylefrancis@gmail.com>
2024-05-22 21:29:43 +0800

    navbar home button

--- components/template/navbar/index.tsx
+++ components/template/navbar/index.tsx
@@ -25,43 +25,48 @@ export function Navbar() {
   return (
     <div className="relative wave bg-secondary h-[70px] spaced-x">
       <div className="flex items-center justify-between h-full gap-spaced translate-y-[3px] relative">
+        {/* middle */}
         <TransitionLink
           href="/"
-          className="relative -translate-x-0 -translate-y-0"
+          className="flex flex-col text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 nav-brand hover:text-primary transition-colors cursor-pointer"
         >
-          <div className="w-10 aspect-square">
-            {/* <Image src={brand} alt="" /> */}
+          <div className="text-3xl font-gopher ">E&B</div>
+          <div className="font-gopher uppercase text-xs leading-[0.8em]">
+            Farm
           </div>
         </TransitionLink>
-        {user ? (
-          <div className="flex ml-auto gap-spaced-sm">
-            <div
-              className={` flex gap-spaced bg-light w-9 md:w-10 aspect-square items-center justify-center rounded-full transition-colors ${
-                pathname.includes("/profile") ? "bg-primary text-light" : ""
-              }`}
-            >
-              <TransitionLink href="/profile">
-                <div>
-                  <i className="text-xl md:text-2xl bi bi-person"></i>
-                </div>
-              </TransitionLink>
+        {/* left */}
+        <div className="ml-auto">
+          {user ? (
+            <div className="flex ml-auto gap-spaced-sm">
+              <div
+                className={` flex gap-spaced bg-light w-9 md:w-10 aspect-square items-center justify-center rounded-full transition-colors ${
+                  pathname.includes("/profile") ? "bg-primary text-light" : ""
+                }`}
+              >
+                <TransitionLink href="/profile">
+                  <div>
+                    <i className="text-xl md:text-2xl bi bi-person"></i>
+                  </div>
+                </TransitionLink>
+              </div>
+              <SimpleButton
+                className="bg-light"
+                onClick={() => {
+                  signOut();
+                  resetOrdersStore();
+                  router.push("/");
+                }}
+              >
+                Log Out
+              </SimpleButton>
             </div>
-            <SimpleButton
-              className="bg-light"
-              onClick={() => {
-                signOut();
-                resetOrdersStore();
-                router.push("/");
-              }}
-            >
-              Log Out
-            </SimpleButton>
-          </div>
-        ) : (
-          <TransitionLink href="/account/login">
-            <SimpleButton className="bg-light">Log In</SimpleButton>
-          </TransitionLink>
-        )}
+          ) : (
+            <TransitionLink href="/account/login">
+              <SimpleButton className="bg-light">Log In</SimpleButton>
+            </TransitionLink>
+          )}
+        </div>
       </div>
     </div>
   );
